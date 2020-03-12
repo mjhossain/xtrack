cb851854b1bce5f603990c69fa0e0e218ab71aef
anupsdesign <anup@gmail.com>
2020-03-12 15:56:32 -0400

    created a landing page for the application

---
+++ landing.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Bootstrap CSS -->
+      
+      <link rel="stylesheet" href="style/bootstrap.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      
+      <link rel="stylesheet" href="style/landing.css">
+      
+      <title>XTrack | Getting Started</title>
+  </head>
+  <body>
+      <header>
+          <a href="#"><div class="logo"></div><span>Track</span></a>
+          <nav>
+              <a href="">About</a>
+              <a href="">Support</a>
+          </nav>
+          <button>Sign Up</button>
+      </header>
+      <section>
+          <div class="center-content">
+              <div class="container-fluid">
+                  <div class="row">
+                      <div class="col-md px-0 left">
+                          <div class="inside-center">
+                              <span>Control</span>
+                              <h1>Take control over your money</h1>
+                              <p>
+                                  It is your money and you should know how and when you are spending it. <br><br>
+                                  With XTrack you can manage your expenses seamlessly &amp; intuitively.
+                              </p>
+                              <button>Get Started</button>
+                          </div>
+                      </div>
+                      <div class="col-md px-0 right">
+                            <div class="carousel-wrap">
+                                <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
+                                    <!-- Indicators -->
+                                    <ol class="carousel-indicators">
+                                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                                    </ol>
+                                    <!-- Wrapper for slides -->
+                                    <div class="carousel-inner">
+                                        <div class="item active">
+                                            <img src="images/checkout.gif" alt="Los Angeles" style="width:100%;">
+                                        </div>
+                                        <div class="item">
+                                            <img src="images/receipt.gif" alt="Chicago" style="width:100%;">
+                                        </div>
+
+                                        <div class="item">
+                                            <img src="images/wallet.gif" alt="Chicago" style="width:100%;">
+                                        </div>
+
+                                    </div>
+                                    <div class="carousel-wrap">
+
+                                    </div>
+                                </div>
+                            </div>                          
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+      
+      <footer>Designed and Developed By <a href="#">Anup</a> &amp; <a href="#">Mohammed</a>.</footer>
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script> 
+      
+  </body>
+</html>
