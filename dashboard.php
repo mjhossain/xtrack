b87f9f2fa82e100c901845327cac188465ea7803
Mohammed J Hossain <mjhossainnyc@gmail.com>
2020-05-04 23:24:58 -0400

    Fix dashboard

---
+++ dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/dash.css">
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <style>
+
+    </style>
+</head>
+
+<body>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script>
+    </script>
+</body>
+
+</html>
