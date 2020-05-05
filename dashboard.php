4391fc1c8459347e832fe30aa7ba1ed7df9370b4
Mohammed J Hossain <mjhossainnyc@gmail.com>
2020-05-05 17:35:26 -0400

    Design started

--- dashboard.php
+++ dashboard.php
@@ -5,16 +5,52 @@
     <title>Dashboard</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style/dash.css">
-    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style/dash.css">
     <style>
 
     </style>
 </head>
 
 <body>
+    <div class="body-wrap">
+        <div class="side-nav">
+            <a href="#">
+                <div class="user-image-wrap">
+                    <div class="user-image"></div>
+                </div>
+            </a>
+            <nav>
+                <a href="#"><button class="active"><img src="images/dashboard-img/home.png" width="40%"></button></a>
+                <a href="#"><button><img src="images/dashboard-img/user.png" width="40%"></button></a>
+                <a href="#"><button><img src="images/dashboard-img/headset.png" width="40%"></button></a>
+                <a href="#"><button><img src="images/dashboard-img/gear.png" width="40%"></button></a>
+            </nav>
+            <button class="logout"><img src="images/dashboard-img/logout.png"  width="40%"></button>
+        </div>
+        <div class="main-content">
+            <div class="margin-fix">
+                <h1>Hello Candice</h1>
+                <div class="mt-5 row justify-content-center">
+                    <div class="col-lg-4 box">
+                        <div class="box1">
 
+                        </div>
+                    </div>
+                    <div class="col-lg-4 box">
+                        <div class="box2">
+
+                        </div>
+                    </div>
+                    <div class="col-lg-4 box">
+                        <div class="box3">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script>
     </script>
